629a88009f55abdbeab2d32f6be44ad4fe3f893f
jChenvan <188939308+jChenvan@users.noreply.github.com>
2025-07-15 16:27:02 -0400

    Setup table

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Icon from '@mdi/react'
+import { mdiPlus } from '@mdi/js'
+
+interface Item {
+  id: number,
+  name: string,
+  group: "PRIMARY" | "SECONDARY",
+  "created_at": string,
+  "updated_at": string,
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [items, setItems] = useState<Item[]>([]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='w-screen h-screen flex justify-center items-center'>
+        <table className='rounded-lg overflow-hidden'>
+          <thead className='bg-blue-700'>
+            <tr>
+              <th className='p-4 text-white'>id</th>
+              <th className='p-4 text-white'>name</th>
+              <th className='p-4 text-white'>group</th>
+              <th className='p-4 text-white'>created</th>
+              <th className='p-4 text-white'>updated</th>
+              <th>
+                <button className='m-4 cursor-pointer hover:opacity-80'><Icon path={mdiPlus} size={1} color={"white"}/></button>
+              </th>
+            </tr>
+          </thead>
+          {!items.length && <tr><td colSpan={6} className='text-center p-4 bg-blue-300'>No items, click "+" to add!</td></tr>}
+        </table>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
